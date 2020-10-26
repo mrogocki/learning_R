0bc1eef6d4695ac5daa73f80424ddc199b7f6260
M. Rogocki <m.rogocki@web.de>
2020-10-26 22:33:36 +0100

    new app

---
+++ app.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(
+      h2("Installation"),
+      p("Shiny is available on CRAN under:"),
+      code("install.packages(Shiny)"),
+      img(src = "rstudio.png", height = 70, width = 200)
+      
+    ),
+  mainPanel(
+    h1("Introducing Shiny"),
+    p("Shiny is a new package from RStudio that makes it ", 
+      em("incredibly easy "), 
+      "to build interactive web applications with R."),
+    br(),
+    p("For an introduction and live examples, visit the ",
+      a("Shiny homepage.", 
+        href = "http://shiny.rstudio.com")),
+    br(),
+    h2("Features"),
+    p("- Build useful web applications with only a few lines of code—no JavaScript required."),
+    p("- Shiny applications are automatically 'live' in the same way that ", 
+      strong("spreadsheets"),
+      " are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser.")
+  )
+    
+    
+    
+  )  
+  )
+  
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
